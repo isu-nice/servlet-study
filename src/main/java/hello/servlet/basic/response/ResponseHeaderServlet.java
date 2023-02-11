4912d45a5e774d968cdb6521e709c66bfa98cf7f
Isu Shin <isu010614@naver.com>
2023-02-11 15:27:52 +0900

    feat: HTTP 응답 메시지 생성

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * http://localhost:8080/response-header
+ *
+ */
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest requset, HttpServletResponse response) throws ServletException, IOException {
+        // [status-line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        // [response-header]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
+
+    }
+}
