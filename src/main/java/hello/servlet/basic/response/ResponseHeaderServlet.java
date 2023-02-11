712c7a7cd2506052b855b4a729a207782b58457d
Isu Shin <isu010614@naver.com>
2023-02-11 15:33:39 +0900

    feat: cookie 편의 메서드

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,6 +28,7 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         // [Header 편의 메서드]
         content(response);
+        cookie(response);
 
         PrintWriter writer = response.getWriter();
         writer.println("ok");
@@ -39,5 +41,13 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
     }
 }
